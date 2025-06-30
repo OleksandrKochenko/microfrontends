0cf5722f1699aa268b193e94c6bdb505f71c9b64
Alex Kochenko <kochenko.fop@gmail.com>
2025-06-30 11:09:04 +0200

    upd: navbar links from root's props

--- packages/navbar/src/root.component.tsx
+++ packages/navbar/src/root.component.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { links } from "./root.helper";
+import { NavLink } from "./root.helper";
 import { Link, BrowserRouter } from "react-router-dom";
 
-export default function Root(_props: Record<string, unknown>) {
+export default function Root(props: Record<string, unknown>) {
+  const navLinks = props.navbarRoutes as NavLink[];
+
   return (
     <BrowserRouter>
       <div className="h-16 flex items-center justify-between px-6 bg-primary text-white">
         <div className="flex items-center justify-between">
-          {links.map((link) => (
+          {navLinks.map((link: NavLink) => (
             <Link key={link.href} className="p-6" to={link.href}>
               {link.name}
             </Link>
--- packages/navbar/src/root.helper.ts
+++ packages/navbar/src/root.helper.ts
@@ -2,14 +2,3 @@ export interface NavLink {
   name: string;
   href: string;
 }
-
-export const links: NavLink[] = [
-  {
-    name: "Requisitions",
-    href: "/requisitions",
-  },
-  {
-    name: "Vendors",
-    href: "/vendors",
-  },
-];
